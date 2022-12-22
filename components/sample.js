596fd1b9062662d2924974785ea3068c5f92e9e2
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-12-22 21:01:50 +0800

    Add sample data for homepage

---
+++ components/sample.js
@@ -0,0 +1,514 @@
+const sample = [
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
+      },
+    ],
+  },
+];
+
+export default sample;
