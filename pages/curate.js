d7e835210a4d94a1005617e1510291b80f54b04c
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-12-22 21:04:30 +0800

    Update homepage and curate page
    Enable metamask connection

--- pages/curate.js
+++ pages/curate.js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { alchemy } from "../components/alchemy";
-import { Grid, GridItem } from "@chakra-ui/layout";
+import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/layout";
+import alchemy from "../components/alchemy";
+import Thumbnail from "../components/thumbnail";
 
 function Curator() {
   const router = useRouter();
@@ -14,24 +15,29 @@ function Curator() {
         router.push("/");
         throw new Error("Missing account. Please connect your MetaMask first!");
       }
-      const nfts = await alchemy.nft.getNftsForOwner(account); // norde.eth
-      setNFTs([...nfts.ownedNfts]);
-      console.log(NFTs);
+      alchemy.nft
+        .getNftsForOwner("0x6f69DCf34092CA364ff179a504Dc19d876E86eae")
+        .then((nfts) => {
+          console.log(nfts);
+          setNFTs([...nfts.ownedNfts]);
+        });
     } catch (error) {
       console.log(error.message);
     }
   };
-  useEffect(() => fetchNFTs);
+  useEffect(() => fetchNFTs, [NFTs]);
 
   return (
-    <Grid
-      templateRows="repeat(4, 1fr)"
-      templateColumns="repeat(3, 1fr)"
-      gap={4}
-    >
-      <GridItem rowSpan={2} colSpan={1} bg="tomato" />
-      <GridItem colSpan={1} bg="papayawhip" />
-      <GridItem colSpan={1} bg="papayawhip" />
+    <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+      <GridItem rowSpan={2} colSpan={1}>
+        <Box>
+          <Heading>Admire</Heading>
+          <Text>Click Curate Share</Text>
+        </Box>
+      </GridItem>
+      {NFTs.map((nft) => {
+        return <Thumbnail metadata={nft} />;
+      })}
     </Grid>
   );
 }
