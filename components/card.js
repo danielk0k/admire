f00b75832247d73c258b5814cd1a333de50a18cb
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-12-22 21:01:31 +0800

    Components: add basic design for nft thumbnails and cards

--- components/card.js
+++ components/card.js
@@ -1,5 +1,53 @@
-function Card({ metadata }) {
-  return;
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  HStack,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
+
+function Card({ metadata, isOpen, onClose }) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="full">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{metadata.title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <HStack>
+            <Image
+              src={
+                metadata.media.length === 0
+                  ? "https://place-hold.it/300"
+                  : metadata.media[0].gateway
+              }
+              width="400"
+              height="400"
+            />
+            <Box>
+              <Text>{metadata.description}</Text>
+              <Text>{metadata.tokenId}</Text>
+              <Text>{metadata.tokenType}</Text>
+              <Text>{metadata.contract.address}</Text>
+              <Text>{metadata.rawMetadata.attribute}</Text>
+            </Box>
+          </HStack>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
 
 export default Card;
--- components/thumbnail.js
+++ components/thumbnail.js
@@ -1,7 +1,24 @@
+import { GridItem, useDisclosure } from "@chakra-ui/react";
 import Image from "next/image";
+import Card from "./card";
 
 function Thumbnail({ metadata }) {
-  return;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <GridItem colSpan={1}>
+      <Image
+        src={
+          metadata.media.length === 0
+            ? "https://place-hold.it/300"
+            : metadata.media[0].gateway
+        }
+        onClick={onOpen}
+        width="400"
+        height="400"
+      />
+      <Card metadata={metadata} isOpen={isOpen} onClose={onClose}></Card>
+    </GridItem>
+  );
 }
 
 export default Thumbnail;
