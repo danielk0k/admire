7c35315aeb9a197f664d62c67681f569220d65ba
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-12-22 22:40:25 +0800

    Customise default theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,10 +1,40 @@
 import { extendTheme } from "@chakra-ui/react";
+import { modalAnatomy as parts } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(parts.keys);
+
+const baseStyle = definePartsStyle({
+  dialog: {
+    borderRadius: "20px",
+    background: "#090d10",
+    padding: "10px",
+  },
+});
+
+const modalTheme = defineMultiStyleConfig({
+  baseStyle,
+});
 
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const styles = {
+  global: {
+    "html, body": {
+      margin: "30px",
+      background: "#090d10",
+    },
+  },
+};
+
+const theme = extendTheme({
+  config,
+  styles,
+  components: { Modal: modalTheme },
+});
 
 export default theme;
