2f1d2e15052a3645fe6f7174ad8f13242a5b8b48
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-12-27 14:10:20 +0800

    Rename environment variables

--- components/alchemy.js
+++ components/alchemy.js
@@ -1,7 +1,7 @@
 import { Alchemy, Network } from "alchemy-sdk";
 
 const settings = {
-  apiKey: process.env.ALCHEMY_API,
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API,
   network: Network.ETH_MAINNET,
 };
 
