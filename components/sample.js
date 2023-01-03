14badc9bec944def1d23bc24127fbb98d90d5e10
Daniel <70556073+danielk0k@users.noreply.github.com>
2023-01-03 10:37:09 +0800

    Reorder sample nfts

--- components/sample.js
+++ components/sample.js
@@ -1,57 +1,6 @@
 const sample = [
   {
     contract: {
-      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
-    },
-    tokenId: "7139",
-    tokenType: "ERC721",
-    title: "Kanpai Panda",
-    description:
-      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
-    rawMetadata: {
-      attributes: [
-        {
-          value: "Optimism",
-          trait_type: "Birth-Chain",
-        },
-        {
-          value: "Red",
-          trait_type: "Body",
-        },
-        {
-          value: "Dragon",
-          trait_type: "Tattoo",
-        },
-        {
-          value: "Naked",
-          trait_type: "Clothes",
-        },
-        {
-          value: "Double Gold",
-          trait_type: "Jewelry",
-        },
-        {
-          value: "Brown Beard",
-          trait_type: "Mouth",
-        },
-        {
-          value: "Stunned",
-          trait_type: "Mood",
-        },
-        {
-          value: "Blue Headband",
-          trait_type: "Head",
-        },
-      ],
-    },
-    media: [
-      {
-        gateway: "/images/panda.png",
-      },
-    ],
-  },
-  {
-    contract: {
       address: "0x123b30e25973fecd8354dd5f41cc45a3065ef88c",
     },
     tokenId: "1052",
@@ -232,6 +181,57 @@ const sample = [
     media: [
       {
         gateway: "/images/friend.png",
+      },
+    ],
+  },
+  {
+    contract: {
+      address: "0xacf63e56fd08970b43401492a02f6f38b6635c91",
+    },
+    tokenId: "7139",
+    tokenType: "ERC721",
+    title: "Kanpai Panda",
+    description:
+      "Kanpai Pandas is a collection of 10,000 randomly generated and stylistically curated hand-drawn NFTs that exist on the Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum, and Optimism chains",
+    rawMetadata: {
+      attributes: [
+        {
+          value: "Optimism",
+          trait_type: "Birth-Chain",
+        },
+        {
+          value: "Red",
+          trait_type: "Body",
+        },
+        {
+          value: "Dragon",
+          trait_type: "Tattoo",
+        },
+        {
+          value: "Naked",
+          trait_type: "Clothes",
+        },
+        {
+          value: "Double Gold",
+          trait_type: "Jewelry",
+        },
+        {
+          value: "Brown Beard",
+          trait_type: "Mouth",
+        },
+        {
+          value: "Stunned",
+          trait_type: "Mood",
+        },
+        {
+          value: "Blue Headband",
+          trait_type: "Head",
+        },
+      ],
+    },
+    media: [
+      {
+        gateway: "/images/panda.png",
       },
     ],
   },
